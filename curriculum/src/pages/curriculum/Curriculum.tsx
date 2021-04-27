2da7d65bb7fd93a3c705f19124192aa06631450f
Gabriel Longarete Mandelli <gabriel-mandelli@hotmail.com>
2021-04-27 10:09:14 -0300

    refactor: :art: Create components

--- curriculum/src/pages/curriculum/Curriculum.tsx
+++ curriculum/src/pages/curriculum/Curriculum.tsx
@@ -1,33 +1,12 @@
 import React from 'react'
 import staticData from '../../data/personalData.json'
+import Header from '../../components/header/Header';
 import './Curriculum.css';
 
 function Curriculum(){
     return (
         <body>
-            <div>
-                <nav className="navbar navbar-default" id="">
-                    <div className="container">
-                        <div className="navbar-header">
-                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                            </button>
-                        </div>
-                        <div className="navbar-brand">{staticData.profile.name}</div>
-                        <div className="collapse navbar-collapse" id="myNavbar">
-                            <ul className="nav navbar-nav navbar-right">
-                                <li><a href="#educations">EDUCATION</a></li>
-                                <li><a href="#skills">TECHNICAL SKILLS</a></li>
-                                <li><a href="#experiences">EXPERIENCE</a></li>
-                                <li><a href="#projects">PROJECTS</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+            <Header></Header>
             <div id="profile" className="container-fluid bg-1 text-center">
                 <h1 className="margin">{staticData.profile.name}</h1>
                 <div className="item1">
