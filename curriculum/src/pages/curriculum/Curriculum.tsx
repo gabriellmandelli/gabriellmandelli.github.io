7e56a7b42fca97950a2563bf9bda60eeb721f5f1
Gabriel Longarete Mandelli <gabriel-mandelli@hotmail.com>
2021-04-27 10:13:49 -0300

    Add education component

--- curriculum/src/pages/curriculum/Curriculum.tsx
+++ curriculum/src/pages/curriculum/Curriculum.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import staticData from '../../data/personalData.json'
 import Header from '../../components/header/Header';
 import Profile from '../../components/profile/Profile';
+import Education from '../../components/education/Education';
+
 import './Curriculum.css';
 
 function Curriculum(){
@@ -9,26 +11,7 @@ function Curriculum(){
         <body>
             <Header/>
             <Profile/>
-            <div id="educations">
-                <div>
-                    {staticData.educations.map( education => {
-                        return (
-                            <div className="container-fluid bg-2 text-center ftco-section">
-                                <h2 className="margin ftco-animate" id="education_section">Education</h2>
-                                <div><h2><b>{education.name}</b></h2></div>
-                                <div><h3>{education.from}</h3></div>
-                                <div>Andress: <b>{education.andress}</b></div>
-                                <div>Duration: <b>{education.duration}</b></div>
-                                <div>{education.type} at <b>{education.endAt}</b></div>
-                                <div>
-                                    <h4 id="coursework"><b>Areas of expertise:</b></h4>
-                                    <p>{education.details}</p>
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
+            <Education/>
             <div id="skills">
                 <div className="container-fluid bg-3 text-center">
                     <h3 className="margin" id="skills">TECHNICAL SKILLS</h3>
